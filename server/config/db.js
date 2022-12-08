7c19e2152707ca2f8887ce1d67d66beeb3189bbc
malihaafs <mafsa@my.centennialcollege.ca>
2022-12-09 02:13:58 +0600

    Initial Commit

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+/*
+file name: db.js
+author: Maliha Afsa
+student num: 301223227
+web app name: Comp 229 Midterm
+*/
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  URI:"mongodb+srv://MalihaAfsa:Maliha1358$@cluster0.78gh2wx.mongodb.net/?retryWrites=true&w=majority"
+
+};
+
